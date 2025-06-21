0c2491c2d32e6814a418fb131598f4121454c975
steampunkWizard <elijahanelson2nd@gmail.com>
2025-06-21 18:07:42 -0400

    feat: implement opponent retrieval logic in OwnedGameObject

--- src/tcg/classes/OwnedGameObject.ts
+++ src/tcg/classes/OwnedGameObject.ts
@@ -7,9 +7,23 @@ export type OwnedGameObjectArgs = {
 
 export default class OwnedGameObject extends GameObject {
   public owner: Character;
+  private _cachedOpponent: Character | null = null;
 
   constructor(args: OwnedGameObjectArgs & GameObjectArgs) {
     super(args);
     this.owner = args.owner;
+  }
+
+  public get opponent(): Character {
+    if (this._cachedOpponent) {
+      return this._cachedOpponent;
+    }
+    this._cachedOpponent =
+      this.engine.state.players.find((player) => player.id !== this.owner.id) ??
+      null;
+    if (!this._cachedOpponent) {
+      throw new Error("Opponent not found - incomplete game state");
+    }
+    return this._cachedOpponent;
   }
 }

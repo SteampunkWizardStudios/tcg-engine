b46a2ae45950dc50bafc565a5ed9910c77f60ff6
steampunkWizard <elijahanelson2nd@gmail.com>
2025-06-22 17:48:01 -0400

    empowerment next

--- src/tcg/classes/Card.ts
+++ src/tcg/classes/Card.ts
@@ -4,8 +4,11 @@ import CardData from "@type/CardData";
 
 export type CardArgs = { cardData: CardData };
 
+const empowerBoost = 0.2;
+
 export default class Card extends OwnedGameObject {
   public readonly cardData: CardData; // for accessing things name that aren't specific to the instance
+  public empowerLevel: number = 0;
 
   constructor(args: CardArgs & OwnedGameObjectArgs & GameObjectArgs) {
     super(args);

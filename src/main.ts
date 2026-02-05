5979e44d488c0dc784c7a5b7f03f88ea48089c82
steampunkWizard <108240403+SteampunkWizardStudios@users.noreply.github.com>
2026-02-05 18:33:45 +0000

    remove old system stuff

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,14 @@
-import Engine from "@class/Engine";
-import Frieren from "@character/frieren/Frieren";
+type GameState = { turn: number };
 
-console.log("Welcome to TCG Engine");
-const engine = new Engine({});
-const { create, state } = engine;
+type Card = {
+  name: string;
+  description: (state: GameState) => string;
+  onPlay: (state: GameState) => void;
+};
 
-// for testing only, not the example usage
-
-const character = create.character({
-  id: "1263256360825524315",
-  characterData: Frieren,
-});
-
-const character2 = create.character({
-  id: "1263256360825524316",
-  characterData: Frieren,
-});
-
-character.on("cardAddedToDeck", ({ card }) => {
-  console.log(
-    `Card added to ${character.name}'s deck: ${card.name}`
-  );
-});
-
-character.on("cardPlayed", ({ card }) => {
-  console.log(
-    `${character.name} played card: ${card.title}`
-  );
-});
-
-// simulate playing a card
-
-character.hand[0] = character.deck[0];
-character.selectCard(character.hand[0]);
-character.activeCards.push(character.selectedCard!);
-
-// test empowerment
-
-character.selectedCard!.empowerLevel = 2;
-console.log(character.selectedCard!.print());
-character.playSelectedCard();
-console.log(`character2's health: ${character2.stats.health}/${character2.characterData.baseStats.health}`)
+const fireball = {
+  name: "Fireball",
+  description: (state: GameState) =>
+    `Deal ${state.turn * 2 + 3} damage to all enemies.`,
+  onPlay: (state: GameState) => {},
+} satisfies Card;
--- src/tcg/types/Stats.ts
+++
@@ -1,5 +0,0 @@
-export type Stat = "health" | "attack" | "defense" | "speed";
-
-type Stats = Record<Stat, number>;
-
-export default Stats;
